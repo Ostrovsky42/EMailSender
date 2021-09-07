b83d50de5cf5130f51faa0116065411aedd3aa21
Sergey <tootoo9723@gmail.com>
2021-09-07 22:15:08 +0300

    Connected variables via launchSetting

--- EMailSender/EmailWorker/Program.cs
+++ EMailSender/EmailWorker/Program.cs
@@ -1,7 +1,10 @@
 using EmailWorker.Service;
 using EmailWorker.Settings;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RatesApi.Extensions;
+using System.IO;
 
 namespace EmailWorker
 {
@@ -9,14 +12,22 @@ namespace EmailWorker
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configuration = CreateConfiguratuion();
+            configuration.SetEnvironmentVariableForConfiguration();
+            CreateHostBuilder(args, configuration).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        public static IConfiguration CreateConfiguratuion() =>
+            new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                                      .AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: true)
+                                      .Build();
+
+        public static IHostBuilder CreateHostBuilder(string[] args, IConfiguration configuration) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.Configure<EmailConfig>(hostContext.Configuration.GetSection("Gmail"));
+                    services.AddOptions<EmailConfig>().Bind(configuration.GetSection(nameof(EmailConfig)));
                     services.AddTransient<IEMailSenderService, EMailSenderService>();
                     services.AddHostedService<Worker>();
                 });
