12c6edb1d25f45167ea76b9d66aaa4213b3a254c
Eugene <Zhekul.90@gmail.com>
2021-09-12 10:52:24 +0300

    refactor Publisher.Program

--- EMailSender/Publisher/Program.cs
+++ EMailSender/Publisher/Program.cs
@@ -19,30 +19,17 @@ namespace Publisher
             {
                 while (true)
                 {
-                    var amount = await Task.Run(() =>
+                    var subject = await getConsoleText("subject");
+                    var body = await getConsoleText("body");
+                    var displayName = await getConsoleText("displayName");
+                    var listMailAddresses = await getConsoleText("listMailAddresses");
+
+                    await busControl.Publish<MailExchangeModel>(new
                     {
-                        Console.WriteLine("Enter amount (or quit to exit)");
-                        Console.Write("> ");
-                        return Console.ReadLine();
-                    });
-
-                    if ("quit".Equals(amount, StringComparison.OrdinalIgnoreCase))
-                        break;
-
-                    var mailAddresses = await Task.Run(() =>
-                    {
-                        Console.WriteLine("Enter mailAddresses (or quit to exit)");
-                        Console.Write("> ");
-                        return Console.ReadLine();
-                    });
-
-                    if ("quit".Equals(mailAddresses, StringComparison.OrdinalIgnoreCase))
-                        break;
-
-                    await busControl.Publish<MailTransactionExchangeModel>(new
-                    {
-                        Amount = amount,
-                        MailAddresses = mailAddresses
+                        Subject = subject,
+                        Body = body,
+                        DisplayName = displayName,
+                        ListMailAddresses = listMailAddresses
                     });
                 }
             }
@@ -50,6 +37,16 @@ namespace Publisher
             {
                 await busControl.StopAsync();
             }
+        }
+
+        private static async Task<string> getConsoleText(string text)
+        {
+            return await Task.Run(() =>
+            {
+                Console.WriteLine($"Enter { text} (or quit to exit)");
+                Console.Write("> ");
+                return Console.ReadLine();
+            });
         }
     }
 }
