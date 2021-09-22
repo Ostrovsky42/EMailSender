98a3c61ad47bbe231c56e52c622cf3530ba24a07
Eugene <Zhekul.90@gmail.com>
2021-09-22 21:54:27 +0300

    correct Publisher

--- EMailSender/Publisher/IMailExchangeModel.cs
+++ EMailSender/Publisher/IMailExchangeModel.cs
@@ -7,5 +7,7 @@
         public string DisplayName { get; set; }
         public string MailAddresses { get; set; }
         public bool IsBodyHtml { get; set; }
+        public string Base64String { get; set; }
+
     }
 }
